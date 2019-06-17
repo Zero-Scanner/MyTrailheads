bdafd0b1137b06232424243614c04096cd8974cc
s-samusik <s.samusik@outlook.com>
2019-06-18 01:03:48 +0300

    Build Flexible Apps with Lightning Components

--- force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
+++ force-app/main/default/aura/SimilarProperties/SimilarPropertiesController.js
@@ -1,13 +1,20 @@
 ({
     doInit : function(component, event, helper) {
-        var action = component.get("c.findProperties");
+        var spinner = component.find("spinner");
+        $A.util.removeClass(spinner, "slds-hide");
+        var action = component.get("c.getSimilarProperties");
         action.setParams({
             recordId: component.get("v.recordId"),
-            priceRange: "100000"
+            beds: component.get("v.property.fields.Beds__c.value"),
+            price: component.get("v.property.fields.Price__c.value"),
+            searchCriteria: component.get("v.searchCriteria"),
+            priceRange: parseInt(component.get("v.priceRange"), 10)
         });
         action.setCallback(this, function(response){
             var similarProperties = response.getReturnValue();
             component.set("v.similarProperties", similarProperties);
+            console.log("f: ", component.get("v.similarProperties"));
+            $A.util.addClass(spinner, "slds-hide");
         });
         $A.enqueueAction(action);
     }
